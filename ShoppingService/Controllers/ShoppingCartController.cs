09f44390e07c604a1a9d208f145dbf9a6582835d
Mahesh-MSFT <maksh@microsoft.com>
2017-09-12 11:04:07 +0530

    Add project files.

---
+++ ShoppingService/Controllers/ShoppingCartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingService.Models;
+
+namespace ShoppingService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ShoppingCartController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { ((ShoppingCategoryEnum)new Random().Next(0, 10)).ToString(), Environment.MachineName };
+        }
+    }
+}
---
+++ ShoppingService/Models/ShoppingCategoryEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingService.Models
+{
+    public enum ShoppingCategoryEnum
+    {
+        Electronics,
+        Toys,
+        Clothes,
+        Perfumes,
+        Books,
+        Shoes,
+        Flowers,
+        Furniture,
+        Food,
+        Wines
+    }
+}
